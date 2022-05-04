d79c6c56a2040b18156833dfa4d014764b4ab1f4
elaicajdc <58583516+elaicajdc@users.noreply.github.com>
2022-05-04 21:37:12 +0800

    Front and backend done

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,14 +2,24 @@ import React from 'react'
 import '../input.css'
 
 const Header = () => {
-    
+    const useDate = () => {
+        const [today] = React.useState(new Date());
+        
+        const weekArr = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        const weekDay = weekArr[today.getDay()];
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = today.toLocaleString('default', { month: 'long' });
+        const yyyy = today.getFullYear();
+        
+        return {dd, mm, yyyy, weekDay,};
+    };
     return (
         <div className="bg-color-pattern min-w-full h-72 p-4 box-border mb-16 bg-cover">
             <div className="isolate font-mono text-3xl font-bold text-left text-white mt-20">
                 <div className="grid grid-rows-2 grid-cols-8 grid-flow-col gap-0 ">
-                    <div className="col-span-2 row-span-3 text-6xl text-center font-body">05</div>
-                    <div className="col-span-3 text-2xl text-left">Wednesday</div>
-                    <div className="row-span-2 col-span-3 text-base text-left">April, 2022</div>
+                    <div className="col-span-2 row-span-3 text-6xl text-center font-body">{useDate().dd}</div>
+                    <div className="col-span-3 text-2xl text-left">{useDate().weekDay}</div>
+                    <div className="row-span-2 col-span-3 text-base text-left">{useDate().mm} {useDate().yyyy}</div>
                 </div>
                 <div className="text-left text-3xl ml-4">TODO List</div>
             </div>
